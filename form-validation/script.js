627da6571a3a0d6bc5e32699b30306a8b1ebc844
Bhavik Makwana <bhavikmakwana1993@gmail.com>
2020-02-02 09:08:57 +0530

    Form validation project added

---
+++ form-validation/script.js
@@ -0,0 +1,52 @@
+const form  = document.getElementById('form');
+const username  = document.getElementById('username');
+const email  = document.getElementById('email');
+const password  = document.getElementById('password');
+const password2= document.getElementById('password2');
+
+//show error message
+function showError(input,message){
+    const fromControl = input.parentElement;
+    fromControl.className = 'form-control error';
+    const small =  fromControl.querySelector('small');
+    small.innerHTML = message;
+}
+//show success message
+function showSuccess(input){
+    const formControl =input.parentElement;
+    formControl.className="form-control success";
+}
+
+//email validator
+function isValidEmail(input){
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+//evemt listeners
+form.addEventListener('submit', function(e){
+    e.preventDefault(); 
+    if(username.value === '' ){
+        showError(username,'Username is required');
+    }else{
+        showSuccess(username);
+    }
+    if(email.value === '' ){
+        showError(email,'email is required');
+    }else if(!isValidEmail(email.value)){
+        showError(email,'email is not valid');
+    }else{
+        showSuccess(email);
+    }
+    if(password.value === '' ){
+        showError(password,'password is required');
+    }else{
+        showSuccess(password);
+    }
+    if(password2.value === '' ){
+        showError(password2,'password2 is required');
+    }else{
+        showSuccess(password2);
+    }
+
+});
